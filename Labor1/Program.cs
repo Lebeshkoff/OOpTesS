f7708382e71a5070228f670dfa0e1fe68463d57b
Lebeshkoff <Lebeshkoff@users.noreply.github.com>
2019-09-12 16:29:24 +0300

    Изменено текстовое сообщение в лабораторной работе №1

--- Labor1/Program.cs
+++ Labor1/Program.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Labor0
+namespace Labor1
 {
     class Hello
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("NE hello wrld, because i'm very angry about my life. I'm very lazy and it's my big problem");
 
-            // Keep the console window open in debug mode.
+            // Keep the console window open in debug mode. родительский класс для всех классов это -object метды to string !
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }

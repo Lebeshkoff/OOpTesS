1eb39b5d0f3f4c5aeda474046a5cb45ba56b436b
Lebeshkoff <Lebeshkoff@users.noreply.github.com>
2019-09-12 16:10:29 +0300

    Добавьте файлы проекта.

---
+++ Labor2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labor1
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World git test!!!!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
